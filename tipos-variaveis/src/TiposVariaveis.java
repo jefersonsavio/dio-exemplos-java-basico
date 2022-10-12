f3c6ba74089f7b594e7d3d9ff354aa19cd90811c
jefersonsavio <alukardlegend@outlook.com>
2022-10-12 15:44:45 -0300

    alguns exemplos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+    public static void main(String[] args) {
+
+        String nome = "jeferson savio";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int idade = 5;
+
+        idade = 10;
+
+        System.out.println("nome: " + nome);
+        System.out.println("SAlario: " + salarioMinimo);
+        System.out.println("Casting de numero: " + numeroCurto2);
+        System.out.println("Idade:" + idade);
+
     }
+
 }
